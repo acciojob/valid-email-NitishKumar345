8d7720e4df27a217ec1064ded0ba646903355344
Nitish kumar <109736430+NitishKumar345@users.noreply.github.com>
2025-04-25 12:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function validEmail(str) {
   //your JS code here.
+	if (!str || str.length === 0) return false;
+    
+     
+    if (str.length > 320) return false;
+    
+     
+    const emailRegex = /^[a-zA-Z0-9][a-zA-Z0-9._%+-]*[a-zA-Z0-9]@[a-zA-Z0-9][a-zA-Z0-9-]*(\.[a-zA-Z]{2,})+$/;
+    
+     
+    return emailRegex.test(str) && 
+           str.indexOf('..') === -1 &&  
+           str.split('@')[1].length <= 255;
 }
 
 // Do not change the code below.
